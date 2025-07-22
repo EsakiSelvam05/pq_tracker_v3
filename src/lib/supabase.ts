56c99afa0a674502861d722c6680f0e3bdef0af6
Esaki <85359526+EsakiSelvam05@users.noreply.github.com>
2025-07-22 21:50:11 +0530

    Updated supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://pvnbkejjhrwilehsvvkr.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2bmJrZWpqaHJ3aWxlaHN2dmtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwMTE1OTMsImV4cCI6MjA2NDU4NzU5M30.P_mz5TBcZtfnQ6AMeGGT-pBs3l-DSOOc20-pzbpUpY4';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
